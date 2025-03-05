36f184ef8160c2cf69733c7e64d6d149b2031b2b
jadhavharshh <harshjadhavconnect@gmail.com>
2025-03-06 00:01:25 +0530

    Update LoginForm and SignUpForm for improved text clarity and alignment

--- client/src/components/login-form.tsx
+++ client/src/components/login-form.tsx
@@ -9,10 +9,10 @@ export function LoginForm({
 }: React.ComponentPropsWithoutRef<"form">) {
   return (
     <form className={cn("flex flex-col gap-6", className)} {...props}>
-      <div className="flex flex-col items-center gap-2 text-center">
-        <h1 className="text-2xl font-bold">Login to your account</h1>
-        <p className="text-balance text-sm text-muted-foreground">
-          Enter your email below to login to your account
+      <div className="flex flex-col  gap-2 text-start">
+        <h2 className="text-start md:text-3xl font-bold ">Login Account</h2>
+        <p className="text-iridium md:text-sm">
+          Welcome back! Login to your account
         </p>
       </div>
       <div className="grid gap-6">
