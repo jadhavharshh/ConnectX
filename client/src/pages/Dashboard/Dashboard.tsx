78c7b4511a6d23b888a9fbd5e5e4d12d85655306
Nirvik30 <166740525+Nirvik30@users.noreply.github.com>
2025-03-08 16:49:03 +0000

    Add LogoutButton component for user sign-out functionality

--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,10 +1,12 @@
 import { ModeToggle } from "@/components/mode-toggle"
+import { LogoutButton } from "./Logout-btn"
 
 const Dashboard = () => {
   return (
     <div>
       <h1>TREIAL</h1>
        <ModeToggle />
+       <LogoutButton />
     </div>
   )
 }
