213cc8868b6fc5fb1793994ca7223006910eee53
Harsh Jadhav <harshjadhavconnect@gmail.com>
2025-03-08 09:24:26 +0000

    Update environment variable for Clerk publishable key and integrate Toaster component in main application

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from "@/components/theme-provider"
 import './index.css'
 import App from './App.tsx'
 import { ClerkProvider } from '@clerk/clerk-react'
+import { Toaster } from './components/ui/sonner.tsx'
 
 // Import your Publishable Key
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
@@ -15,6 +16,7 @@ if (!PUBLISHABLE_KEY) {
 createRoot(document.getElementById('root')!).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+      <Toaster />
       <App />
     </ThemeProvider>
   </ClerkProvider>
