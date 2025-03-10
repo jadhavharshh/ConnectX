891dd8c342b5d4074095d06e32d402a8d3e4a47f
Harsh Jadhav <harshjadhavconnect@gmail.com>
2025-03-10 10:44:12 +0000

    Configure the VITE ENV variables in the .env file

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,5 +1,5 @@
-export const HOST = import.meta.env.BACKEND_URL;
+export const HOST = import.meta.env.VITE_BACKEND_URL;
 export const AUTH_API = "api/auth";
 
-export const SEND_SIGNUP_DATA = `http://localhost:5000/${AUTH_API}/send-signup-data`;
+export const SEND_SIGNUP_DATA = `${AUTH_API}/send-signup-data`;
 
