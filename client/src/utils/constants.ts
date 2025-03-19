0420523957ea1052d32217e157c6cb83f6c90e05
Harsh Jadhav <harshjadhavconnect@gmail.com>
2025-03-19 08:27:43 +0000

    Add FETCH_ANNOUNCEMENTS constant to API endpoints

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -6,3 +6,4 @@ export const SEND_SIGNUP_DATA = `${AUTH_API}/send-signup-data`;
 export const DATA_API = "api/data";
 export const FETCH_USER_INFO = `${DATA_API}/fetch-user-info`;
 export const CREATE_ANNOUCEMENT = `${DATA_API}/create-announcement`;
+export const FETCH_ANNOUNCEMENTS = `${DATA_API}/fetch-announcements`;
