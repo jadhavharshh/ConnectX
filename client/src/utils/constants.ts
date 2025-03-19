f143f09632992376faa43d296896037b2fa52613
jadhavharshh <harshjadhavconnect@gmail.com>
2025-03-19 14:58:09 +0530

    Add task management functionality with create and fetch endpoints

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -7,3 +7,5 @@ export const DATA_API = "api/data";
 export const FETCH_USER_INFO = `${DATA_API}/fetch-user-info`;
 export const CREATE_ANNOUCEMENT = `${DATA_API}/create-announcement`;
 export const FETCH_ANNOUNCEMENTS = `${DATA_API}/fetch-announcements`;
+export const CREATE_TASKS = `${DATA_API}/create-task`;
+export const FETCH_TASKS = `${DATA_API}/fetch-task`;
